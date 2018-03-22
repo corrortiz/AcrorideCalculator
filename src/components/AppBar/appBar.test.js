1c34e929678fccfb9b1f435be2365b9d0f52d2c5
corrortiz <a.ortizcrr@gmail.com>
2018-03-22 16:27:54 -0600

    borrando todo

--- src/components/AppBar/appBar.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { HeaderAppBar } from './index';
-
-describe('AppBar Component', () => {
-  it('renders without crashing', () => {
-    shallow(<HeaderAppBar />);
-  });
-});
--- src/components/Layout/layout.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { Layout } from './index';
-
-describe('Layout Component', () => {
-  it('renders without crashing', () => {
-    shallow(<Layout />);
-  });
-});
