99564b4db97a1d3f4c349a80f57bd32244af4f6c
corrortiz <a.ortizcrr@gmail.com>
2018-03-22 17:09:47 -0600

    agggggggggggggg

--- README.md
+++
@@ -1,1 +0,0 @@
-Acroride Client Side APP
--- src/components/Routs/Landing/landing.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { Landing } from './index';
-
-describe('Landing Component', () => {
-  it('renders without crashing', () => {
-    shallow(<Landing />);
-  });
-});
