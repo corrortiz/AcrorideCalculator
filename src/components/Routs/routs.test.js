bd5cd692f0a53005253519b205593cc0b1bb8bc0
corrortiz <a.ortizcrr@gmail.com>
2018-03-22 16:43:17 -0600

    Updates

--- src/components/Routs/routs.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { Routs } from './index';
-
-describe('Routs Component', () => {
-  it('renders without crashing', () => {
-    shallow(<Routs />);
-  });
-});
--- src/store/actions/destinoInicial.test.js
+++
@@ -1,7 +0,0 @@
-import * as destinos from './destinoInicial';
-
-describe('Destino inicial', () => {
-  test('deberia de regresar un destino', () => {
-  });
-
-});
