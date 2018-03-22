7f568b6497cc962dd9b849547d6b2fe18a95314e
corrortiz <a.ortizcrr@gmail.com>
2018-03-22 15:54:17 -0600

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom';
-
-import './sass/main.css';
-
-import Layout from './components/Layout';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(Layout , document.getElementById('root'));
-registerServiceWorker();
